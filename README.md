be42ed51f80ea094496ecb1dc82ba4de63cf5f39
01Fedda <31264563+01Fedda@users.noreply.github.com>
2020-08-09 15:05:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SHNDcoin API
+
+API to build web applications.  
+  
+Simple API that sends HTTP requests using RPC commands.
---
+++ routes/api.js
@@ -0,0 +1,256 @@
+const express = require("express");
+const router = express.Router();
+var request = require("request");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const USER = process.env.RPC_USER;
+const PASS = process.env.RPC_PASSWORD;
+
+const headers = {
+  "content-type": "text/plain;"
+};
+
+/*-----------------------------------------------
+	SHND Port Info:
+	P2P = 29001
+	RPC = 29002
+
+	Api web-link examples:
+	http://IpAddress:7777/api/getblockcount
+	http://IpAddress:7777/api/getblockcount/
+-----------------------------------------------*/
+
+//Test if api is working = http://IpAddress:7777/api/test
+router.get("/test", (req, res) => res.json({ msg: "SHND api is working" }));
+
+//Api calls
+router.get("/getblockcount", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblockcount","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:29002/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getconnectioncount", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getconnectioncount","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:29002/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getdifficulty", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getdifficulty","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:29002/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getblockchaininfo", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblockchaininfo","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:29002/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getmininginfo", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getmininginfo","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:29002/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getpeerinfo", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getpeerinfo","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:29002/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getblock/:hash", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblock","params":["${
+    req.params.hash
+  }"]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:29002/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getblockhash/:index", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblockhash","params":[${
+    req.params.index
+  }]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:29002/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getrawtransaction/:id", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getrawtransaction","params":["${
+    req.params.id
+  }"]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:29002/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/decoderawtransaction/:hex", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"decoderawtransaction","params":["${
+    req.params.hex
+  }"]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:29002/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/sendrawtransaction/:hex", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"sendrawtransaction","params":["${
+    req.params.hex
+  }"]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:29002/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+//Testing for unspent (utxo)
+/*router.get("/listunspent/", (req, res) => {
+  var dataString = `{"jsonrpc": "1.0", "id":"curltext", "method": "listunspent", "params": [2, 9999999, [\"STjxGBLeTd8Anu8a6E7bVu4Bfert7TpvJr\",\"SQ7StoZ2jf6rVLtccAvjyCp6pimnjkeqNa\"] , true, { "minimumAmount": 0.005 } ] }`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:29002/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});*/
+
+module.exports = router;
